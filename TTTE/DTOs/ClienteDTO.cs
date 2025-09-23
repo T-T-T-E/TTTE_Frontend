54a0ed631820e3999b2f7b366ef1aad83475559f
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-09-23 10:09:57 -0600

    se agrego diseño al login y se creo el funcion para crear nuevos clientes.

---
+++ TTTE/DTOs/ClienteDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TTTE.DTOs
+{
+    public class ClienteCreateDTO
+    {
+        [JsonPropertyName("nombre_completo")]
+        public string? nombreCompleto { get; set; }
+
+        [JsonPropertyName("email")]
+        public string? email { get; set; }
+
+        [JsonPropertyName("telefono")]
+        public string? telefono { get; set; }
+
+        [JsonPropertyName("password")]
+        public string? password { get; set; }
+    }
+}
