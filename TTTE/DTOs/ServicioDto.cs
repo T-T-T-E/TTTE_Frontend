3fb023c8bd9ecc4e23d4ef47002a85de1497bbf4
Juan_Carlos_Vásquez_López <vl25001@esfe.agape.edu.sv>
2025-09-24 10:00:56 -0600

    Se creo los servicios y componente de servicios y se configuro el program

---
+++ TTTE/DTOs/ServicioDto.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TTTE.DTOs
+{
+    public class ServicioDto
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("nombre_servicio")]
+        public string NombreServicio { get; set; } = string.Empty;
+
+        [JsonPropertyName("precio")]
+        public decimal Precio { get; set; } 
+
+        [JsonPropertyName("foto_servicio")]
+        public string? FotoServicio { get; set; }
+    }
+
+    public class CrearServicioDto
+    {
+        [JsonPropertyName("nombre_servicio")]
+        public string NombreServicio { get; set; } = string.Empty;
+
+        [JsonPropertyName("precio")]
+        public decimal Precio { get; set; }
+
+        [JsonPropertyName("foto_servicio")]
+        public string? FotoServicio { get; set; }
+    }
+}
